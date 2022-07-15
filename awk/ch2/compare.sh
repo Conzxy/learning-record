2c55365dda5d1e7ccdb20ee04d2cfe68a7908b32
Conzxy <1967933844@qq.com>
2022-07-16 01:14:38 +0800

    single quote

--- awk/ch2/compare.sh
+++ awk/ch2/compare.sh
@@ -2,10 +2,10 @@
 echo "Compare test"
 echo
 
-echo "\$3/\$2 >= 0.5"
+echo '$3/$2 >= 0.5: '
 awk 'BEGIN { FS = "\t" } $3/$2 >= 0.5' countries
 echo
 
-echo "\$0 >= "M""
+echo '$0 >= "M": '
 awk 'BEGIN { FS = "\t" } $0 >= "M"' countries
 echo
--- awk/ch2/regex_test.sh
+++ awk/ch2/regex_test.sh
@@ -11,10 +11,31 @@ awk '/ Asia /' countries
 echo
 
 
-echo "\$0 !~ / Asia /: "
+echo '$0 !~ / Asia /: '
 awk '$0 !~ / Asia /' countries
 echo
 
-echo "(\$1 ~ /Japan|England/) || (\$4 ~ /Asia|North America/)"
+echo '($1 ~ /Japan|England/) || ($4 ~ /Asia|North America/)'
 awk 'BEGIN { FS = "\t" } ($1 ~ /Japan|England/) || ($4 ~ /Asia|North America/)' countries
 echo
+
+echo "[Only number]"
+awk '/^[0-9]+$/' number_test
+echo
+
+echo "[Only alphabet]"
+awk '/^[A-Za-z]+$/' number_test
+echo
+
+echo "[Decimal, sign and score is optional]: "
+#awk '/^(\+|-)?[0-9]+\.?[0-9]*$/' number_test
+awk '/^[+-]?[0-9]+\.?[0-9]*$/' number_test
+echo
+
+echo "float point number, sign and exponent is optional"
+awk '/^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)([eE][+-][0-9]+)?$/' number_test
+echo
+
+echo "One letter and it following zero or more letter or digits"
+awk '/^[A-Za-z][A-Za-z0-9]*$/' number_test
+echo 
